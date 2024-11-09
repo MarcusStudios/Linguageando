37e911cd38245f8664af24d2f6dd5633abca571b
MarcusStudios <marcuseduardo846@gmail.com>
2024-11-09 12:15:12 -0300

    d

--- src/pages/boca/index.js
+++ src/pages/boca/index.js
@@ -1,37 +1,107 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, FlatList, Image, StyleSheet, Dimensions, TouchableOpacity, Modal, Animated } from 'react-native';
+import * as Font from 'expo-font';
 
-export default function Boca() {
+const { width, height } = Dimensions.get('window');
+
+export default function Boca({ navigation }) {
     const [images, setImages] = useState([]);
     const [page, setPage] = useState(1);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedText, setSelectedText] = useState('');
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+    const [fontLoaded, setFontLoaded] = useState(false);
 
-    // Função para simular o carregamento de mais imagens (você pode adaptar para buscar de uma API)
-    const loadImages = () => {
-        const newImages = Array.from({ length: 10 }).map((_, index) => ({
-            id: index + page * 10,
-            uri: `https://picsum.photos/200/300?random=${index + page * 10}`
-        }));
-        setImages(prevImages => [...prevImages, ...newImages]);
-        setPage(prevPage => prevPage + 1);
-    };
-
-    // Carregar as imagens iniciais
     useEffect(() => {
+        const loadFont = async () => {
+            await Font.loadAsync({
+                'CustomFont': require("../../assets/fonts/JetBrainsMono-Bold.ttf"), // Substitua pelo caminho correto da fonte
+            });
+            setFontLoaded(true);
+        };
+        loadFont();
         loadImages();
     }, []);
 
+    const loadImages = () => {
+        const newImages = [
+            { id: 1, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 1' },
+            { id: 2, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 2' },
+            { id: 3, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 3' },
+            { id: 4, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 4' },
+            { id: 5, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 5' },
+            { id: 6, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 6' },
+            { id: 7, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 7' },
+            { id: 8, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 8' },
+            { id: 9, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 9' },
+            { id: 10, uri: require('../../assets/ms.jpg'), description: 'Descrição da Imagem 10' },
+        ];
+        setImages(newImages);
+    };
+
+    const openImage = (uri, description) => {
+        setSelectedImage(uri);
+        setSelectedText(description);
+        setModalVisible(true);
+        fadeIn();
+    };
+
+    const fadeIn = () => {
+        fadeAnim.setValue(0);
+        Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    if (!fontLoaded) {
+        return null; // Exibir um indicador de carregamento, se desejar
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Boca!!!!</Text>
+            <View style={styles.headerContainer}>
+                <TouchableOpacity style={styles.exitButton} onPress={() => navigation.goBack()}>
+                    <Text style={[styles.exitButtonText, { fontFamily: 'CustomFont' }]}>Sair</Text>
+                </TouchableOpacity>
+                <Text style={[styles.title, { fontFamily: 'CustomFont' }]}>Galeria - Boca</Text>
+            </View>
+
             <FlatList
                 data={images}
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => (
-                    <Image source={{ uri: item.uri }} style={styles.image} />
+                    <TouchableOpacity onPress={() => openImage(item.uri, item.description)}>
+                        <Image source={item.uri} style={styles.image} />
+                    </TouchableOpacity>
                 )}
-                onEndReached={loadImages}
+                numColumns={2}
+                onEndReached={() => setPage(page + 1)}
                 onEndReachedThreshold={0.5}
             />
+
+            <Modal
+                visible={modalVisible}
+                transparent={true}
+                animationType="fade"
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.modalContainer}>
+                    <TouchableOpacity
+                        style={styles.closeButton}
+                        onPress={() => setModalVisible(false)}
+                    >
+                        <Text style={styles.closeButtonText}>✕</Text>
+                    </TouchableOpacity>
+
+                    <View style={styles.modalContent}>
+                        <Animated.Image source={selectedImage} style={[styles.modalImage, { opacity: fadeAnim }]} />
+                        <Text style={[styles.modalText, { fontFamily: 'CustomFont' }]}>{selectedText}</Text>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 }
@@ -40,16 +110,77 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 16,
-        backgroundColor: '#fff',
+        backgroundColor: '#f2f2f2',
+    },
+    headerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 16,
+    },
+    exitButton: {
+        position: 'absolute',
+        left: 0,
+        padding: 8,
+    },
+    exitButtonText: {
+        fontSize: 16,
+        color: '#ff5555',
     },
     title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
+        fontSize: 28,
+        color: '#333',
+        textAlign: 'center',
     },
     image: {
-        width: '100%',
-        height: 200,
-        marginBottom: 16,
+        width: width / 2 - 24,
+        height: 250,
+        margin: 8,
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#ddd',
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.85)',
+    },
+    modalContent: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalImage: {
+        width: width * 0.9,
+        height: height * 0.7,
+        resizeMode: 'contain',
+        borderRadius: 10,
+    },
+    modalText: {
+        marginTop: 16,
+        fontSize: 18,
+        color: '#fff',
+        textAlign: 'center',
+        paddingHorizontal: 16,
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 40,
+        left: 20,
+        backgroundColor: '#ff5555',
+        width: 40,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        elevation: 10,
+        shadowColor: '#000',
+        shadowOpacity: 0.3,
+        shadowRadius: 6,
+    },
+    closeButtonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
     },
 });
